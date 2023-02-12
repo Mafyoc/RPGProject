176814e0b070074b8fb3bfaecd49f517bd20595e
Mafyoc <mattpanting123@hotmail.co.uk>
2023-02-12 16:28:40 +0000

    refactored main.cpp

--- RPGProject/main.cpp
+++ RPGProject/main.cpp
@@ -6,6 +6,24 @@
 #include<iostream>
 
 int main() {
+	
+	bool characerCreating = true;
+
+	while(characerCreating = true)
+	{
+		std::cout << "Please enter the corresponding number to select a class" << std::endl;
+		std::cout << "\t 1 - Warrior" << std::endl;
+		std::cout << "\t 2 - Priest" << std::endl;
+		std::cout << "\t 3 - Mage" << std::endl;
+		if (getline(std::cin)) {
+		}
+	}
+	
+	
+	
+	
+	
+	/*
 	Player* myWarrior = new Warrior("Joe", Race::HUMAN, 200, 0);
 	Player* myPriest = new Priest("Dan", Race::ELF, 150, 150);
 	Player* myMage = new Mage("Joe", Race::DWARF, 100, 200);
@@ -21,6 +39,7 @@ int main() {
 	myWarrior = nullptr;
 	myPriest = nullptr;
 	myMage = nullptr;
+	*/
 
 	return 0;
 }
